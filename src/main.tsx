7d27a3764b581100853f7f37945caaa8149b477f
Amit Asghar <amitasghar@yahoo.com>
2026-04-26 10:34:42 -0400

    feat(frontend): /admin route renders AdminPage via pathname check

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { AdminPage } from "./components/AdminPage.tsx";
 import "./index.css";
+
+const isAdmin = window.location.pathname.startsWith("/admin");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <AdminPage /> : <App />}
   </React.StrictMode>,
 );
